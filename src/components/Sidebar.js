9be655f541bfe9936cbf43b820aef6286fe79e96
Yuvraj Singh Bhatiya <yvrjbhatiya>
2023-11-04 15:50:25 +0530

    new

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,12 +16,12 @@ const Sidebar = () => {
   };
 
   return (
-    <Link
-      to="/"
+    <div
       className={`${
         isOpen ? "right-0" : "-right-full"
-      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+      } fixed top-0 h-full bg-white shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
+      {/* Header */}
       <div className="flex items-center justify-between py-6 border-b">
         <div className="uppercase text-lg font-semibold">
           Shopping Bag ({itemAmount})
@@ -33,13 +33,16 @@ const Sidebar = () => {
           <IoMdArrowForward className="text-2xl" />
         </div>
       </div>
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
+      {/* Cart items with fixed height and scrolling */}
+      <div className="flex flex-col gap-y-2 h-[400px] overflow-y-auto border-b">
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
-        <div className="flex flex-col gap-y-3 py-4 mt-4">
-          {cartIsEmpty ? (
-            <>
+      </div>
+      {/* Footer (Total and Checkout) */}
+      <div className="flex flex-col gap-y-3 py-4 mt-4">
+        {cartIsEmpty ? (
+          <>
               <div className="text-center mt-10">
                 <img src={EmptyIMG} alt="Empty Cart" className="mx-auto" />
               </div>
@@ -47,36 +50,27 @@ const Sidebar = () => {
                 No items in your bag
               </div>
             </>
-          ) : (
-            // Individual item prices and quantities
-            <div className="mb-4">
-              {cart.map((item) => (
-                <div key={item.id} className="flex justify-between">
-                  <span>{item.name}</span>
-                  <span>₹ {item.price.toFixed(2)}</span>
-                </div>
-              ))}
-            </div>
-          )}
-          {/* Total price */}
-          <div className="flex justify-between items-center mb-4">
+        ) : (
+          <div className="flex w-full justify-between items-center mb-4">
             <div className="font-semibold">
               <span className="mr-2 text-xl">Total :</span>
-              <span className="text-xl"> ₹ {total.toFixed(2)}</span>
+              <span className="text-xl">
+                ₹ {parseFloat(total).toFixed(2)}
+              </span>
             </div>
           </div>
-          {!cartIsEmpty && (
-            <Link
-              to="/checkout"
-              onClick={handleCheckoutClick}
-              className="bg-primary flex p-4 justify-center items-center text-white text-lg w-full font-medium transition duration-150 hover:bg-fadeCustom hover:text-black "
-            >
-              Checkout
-            </Link>
-          )}
-        </div>
+        )}
+        {!cartIsEmpty && (
+          <Link
+            to="/checkout"
+            onClick={handleCheckoutClick}
+            className="bg-primary flex p-4 justify-center items-center text-white text-lg w-full font-medium transition duration-150 hover:bg-fadeCustom hover:text-black "
+          >
+            Checkout
+          </Link>
+        )}
       </div>
-    </Link>
+    </div>
   );
 };
 
